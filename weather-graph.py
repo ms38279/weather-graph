1afdbda8559f11bac50cadc93419c31bcaf3f7f6
ms38279 <ms38279@gmail.com>
2019-03-27 15:47:51 -0700

    add weather graph

---
+++ weather-graph.py
@@ -0,0 +1,25 @@
+from requests import get
+import matplotlib.pyplot as plt
+from dateutil import parser
+
+url= 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getallmeasurements/505307'
+
+weather= get (url).json()
+
+#print weather['items'][0]['ambient_temp']
+
+temperatures=[] #temperature list
+timestamps=[]
+
+for record in weather ['items']:
+	temp= record ['ambient_temp']
+	time=record['reading_timestamp']
+	temperatures.append(temp)
+	timestamps.append(parser.parse(time))
+
+# create a plot of timestamps against temperature and show it
+
+plt.plot(timestamps, temperatures)
+plt.ylabel('Temperature')
+plt.xlabel('Time')
+plt.show()
